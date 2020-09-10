71176d530e81fc07ea1af0f9183960a406c826a8
sihem azibi <s.azibi@live.fr>
2020-09-10 08:54:33 -0500

    Add project files.

---
+++ Data/AsyncInnDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
