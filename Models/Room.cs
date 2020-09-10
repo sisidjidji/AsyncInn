c046fd7031d09fdcf74015a6556299566ee0d9d8
sihem azibi <s.azibi@live.fr>
2020-09-10 10:09:27 -0500

    room model created

---
+++ Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public string name { get; set; }
+        public int Layout { get; set; }
+
+    }
+}
